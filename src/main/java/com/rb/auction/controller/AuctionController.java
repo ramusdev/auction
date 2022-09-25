91829d49e9715aec825b241fd388da498312f339
Roman <ram219219@gmail.com>
2022-09-25 14:06:19 +0300

    Added fix to ui product page

--- src/main/java/com/rb/auction/controller/AuctionController.java
+++ src/main/java/com/rb/auction/controller/AuctionController.java
@@ -1,88 +1,27 @@
 package com.rb.auction.controller;
 
-import com.rb.auction.model.Auction;
 import com.rb.auction.model.AuctionBet;
-import com.rb.auction.model.Product;
-import com.rb.auction.model.User;
-import com.rb.auction.model.view.AuctionView;
 import com.rb.auction.service.InterfaceAuctionService;
 import com.rb.auction.service.InterfaceProductService;
 import com.rb.auction.session.SessionObject;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Set;
-
+@Log4j2
 @Controller
 public class AuctionController {
-
     @Autowired
     InterfaceProductService interfaceProductService;
-
     @Autowired
     InterfaceAuctionService interfaceAuctionService;
-
     @Autowired
     SessionObject sessionObject;
 
     @RequestMapping(value = "/auction/addbet/{id}", method = RequestMethod.POST)
-    public String addBet(@ModelAttribute AuctionBet auctionBet, @PathVariable int id) {
+    public String addBet(@ModelAttribute AuctionBet auctionBet, @PathVariable int id, @RequestParam int postid) {
         this.interfaceAuctionService.addBetToAuction(id, auctionBet);
-
-        // return "redirect:/product/{id}";
-        return "redirect:/main";
-    }
-
-    @RequestMapping(value = "/auction/addauction", method = RequestMethod.POST)
-    public String addAuction(@ModelAttribute AuctionView auctionView) {
-        System.out.println("-----------------------------------------------------");
-        System.out.println("Add auction");
-
-        // this.interfaceAuctionService.addAuction(auctionView);
-
-        return "redirect:/main";
-    }
-
-    @RequestMapping(value = "/auction/addauction", method = RequestMethod.GET)
-    public String addAuctionShow(Model model) {
-        User user = this.sessionObject.getUser();
-        List<Product> products = this.interfaceProductService.getProductsByUserId(user.getId());
-
-        model.addAttribute("rproducts", products);
-        model.addAttribute("rauction", new AuctionView());
-
-        return "addauction";
-    }
-
-    @RequestMapping(value = "/auction/{id}", method = RequestMethod.GET)
-    public String auctionShow(@PathVariable int id, Model model) {
-        this.interfaceAuctionService.updateStatus(id);
-
-        Auction auction = this.interfaceAuctionService.getById(id);
-        // Product product = auction.getProduct();
-        Set<AuctionBet> auctionBets = auction.getAuctionBets();
-        // User user = auction.getUser();
-
-        // this.interfaceAuctionService.sortByDate(auctionBets);
-
-        SessionObject sessionObject = null;
-        if (this.sessionObject.isLogged()) {
-            sessionObject = this.sessionObject;
-        }
-        model.addAttribute("sessions", sessionObject);
-
-        model.addAttribute("rauction", auction);
-        // model.addAttribute("rproduct", product);
-        model.addAttribute("rauctionbet", new AuctionBet());
-        model.addAttribute("rauctionbets", auctionBets);
-        // model.addAttribute("ruser", user);
-
-        return "auction";
+        return "redirect:/product/" + postid;
     }
 }
